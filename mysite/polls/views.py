b851b99297c0671b19b8401cba14ba6b08340aeb
Robert Godlewski <robtechg5@gmail.com>
2022-06-02 18:05:27 -0700

    Added html templates.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,31 @@
 # views files control the logic of how to output the data
-from django.http import HttpResponse
-#from django.shortcuts import render
+from django.http import HttpResponse, Http404
+#from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    #return HttpResponse("Your're looking at question %s." % question_id)
+    #try: question = Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist: raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "Your're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    response = "You're voting on question %s."
+    return HttpResponse(response % question_id)
